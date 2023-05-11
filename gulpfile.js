36dafd85e365737068e7d4571b49db91c4cf0cbf
shiraishi <uno0710unoonu@gmail.com>
2023-05-11 14:46:39 +0900

    gulpfile.jsとpackage.jsonを修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 import gulp from 'gulp';
-import imagemin from 'gulp-imagemin';
+import imagemin, {mozjpeg, optipng, svgo} from 'gulp-imagemin';
 import cleanCSS from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
 
 const minImg = () => {
     return gulp.src('src/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('img'));
+    .pipe(imagemin(
+        [ mozjpeg({ quality: 80 }),
+          optipng({optimizationLevel: 5}),
+          svgo({ plugins: [ {
+            name: 'removeViewBox',
+            active: false
+          }] })
+        ], { verbose: true }
+      )
+    )
+    .pipe(gulp.dest('dist/img'));
 }
 
 const minCss = () => {
-    return gulp.src('src/css/style.css')
+    return gulp.src('src/css/*.css')
+    .pipe(autoprefixer())
     .pipe(cleanCSS())
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('dist/css'));
 }
 
 export default gulp.series(minImg, minCss);
+// export default minImg;
+// export default minCss;
